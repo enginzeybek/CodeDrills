d5343e8f1e96906468c6d3681c286dc1d40331a6
Engin Zeybek <engin_zeybek.tb@hotmail.com>
2026-07-22 19:13:07 +0300

    C# Count Digits in a String

---
+++ C# Count Digits in a String/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Bir metin giriniz.");
+string sentence = Console.ReadLine();
+if (string.IsNullOrWhiteSpace(sentence))
+{
+    Console.WriteLine("Boş geçmeyiniz");
+    return;
+}
+
+int count = 0;
+
+foreach (var item in sentence)
+{
+    if (char.IsDigit(item))
+    {
+        count++;
+    }
+}
+
+Console.WriteLine($"Digit count: {count}");
