38bce9a72cf71be450987cedaddd369c3bfc71ba
Engin Zeybek <engin_zeybek.tb@hotmail.com>
2026-08-03 19:54:16 +0300

    C# Count Words

---
+++ C# Count Words/Program.cs
@@ -0,0 +1,23 @@
+﻿while(true)
+{
+    Console.WriteLine("Bir cümle giriniz");
+    string sentence = Console.ReadLine();
+    if (string.IsNullOrWhiteSpace(sentence))
+    {
+        Console.WriteLine("Boş geçmeyiniz");
+        return;
+    }
+    sentence = sentence.ToLower();
+
+    string[] sentenceArray = sentence.Split(" ");
+
+    int count = 0;
+
+    for (int i = 0; i < sentenceArray.Length; i++)
+    {
+        count++;
+    }
+
+    Console.WriteLine($"Word count: {count}");
+
+}
